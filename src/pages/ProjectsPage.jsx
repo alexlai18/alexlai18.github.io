a0745831bd7f2a65d334229af874e4e494fc8062
Alexander <alexander.l.lai@student.unsw.edu.au>
2023-05-20 17:46:28 +1000

    feat: Started projects page

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { orange } from '@mui/material/colors';
+import { Typography, Stack, Divider, Table, TableContainer, Paper, TableRow, TableCell, TableBody } from '@mui/material';
 
 function ProjectsPage() {
   useEffect(() => {
@@ -7,8 +8,30 @@ function ProjectsPage() {
     document.body.style.backgroundColor = colour;
   }, []);
 
+  rows = [
+  ]
+
   return (
     <>
+      <Stack direction='row' justifyContent='center'>
+        <Typography variant='h2'>My Projects!</Typography>
+      </Stack>
+      <br/>
+      <Divider/>
+      <br/>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="project table">
+          <TableRow>
+            <TableCell>Project Name</TableCell>
+            <TableCell>Year Created</TableCell>
+            <TableCell>Languages Used</TableCell>
+          </TableRow>
+          <TableBody>
+
+          </TableBody>
+        </Table>
+      </TableContainer>
+
       Should write about UNSW projects like Microsoft Streams, DungeonMania, Lurkforwork
       Do a little dialog thing to showcase this
     </>
